4c7dc40a6e0eb1d823d972d93e122cfc36ce0108
marksharkpark <39388430+marksharkpark@users.noreply.github.com>
2021-11-22 12:19:16 -0800

    Sample UI
    Includes Firebase authentication.

---
+++ DashCam/Views/CameraViewController.swift
@@ -0,0 +1,18 @@
+//
+//  CameraViewController.swift
+//  DashCam
+//
+//  Created by Mark Park on 11/22/21.
+//
+
+import Foundation
+import UIKit
+import Firebase
+
+class CameraViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
---
+++ DashCam/Views/LoginViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ViewController.swift
+//  DashCam
+//
+//  Created by Mark Park on 10/29/21.
+//
+
+import UIKit
+import SwiftUI
+import Firebase
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var signInButton: UIButton!
+    @IBOutlet weak var registerButton: UIButton!
+    @IBOutlet weak var passwordTextField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func signInButtonPressed(_ sender: Any) {
+        // Optional chaining
+        if let email = emailTextField.text,  let password = passwordTextField.text{
+            // Begin Firebase Authentication
+            Auth.auth().signIn(withEmail: email, password: password) { authResult, error in
+                if let e = error {
+                    let message = e.localizedDescription
+                    let alert = UIAlertController(title: "Oh Boy!", message: message, preferredStyle: .alert)
+                    alert.addAction(UIAlertAction(title: "Try Again", style: .default, handler: nil))
+                    self.present(alert, animated: true, completion: nil)
+                } else {
+                    self.performSegue(withIdentifier: "LoginSegue", sender: self)
+                }
+            }
+        }
+    }
+    
+
+    @IBAction func registerButtonPressed(_ sender: Any) {
+        // Optional chaining
+        if let email = emailTextField.text,  let password = passwordTextField.text{
+            // Begin Firebase Authentication
+            Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+                if let e = error {
+                    let message = e.localizedDescription
+                    let alert = UIAlertController(title: "Oh Boy!", message: message, preferredStyle: .alert)
+                    alert.addAction(UIAlertAction(title: "Try Again", style: .default, handler: nil))
+                    self.present(alert, animated: true, completion: nil)
+                } else {
+                    self.performSegue(withIdentifier: "LoginSegue", sender: self)
+                }
+            }
+        }
+    }
+}
+
