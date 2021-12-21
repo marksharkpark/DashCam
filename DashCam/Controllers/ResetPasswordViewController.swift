c71741d932343a888a321c1f02d7fee5762d7ddd
marksharkpark <39388430+marksharkpark@users.noreply.github.com>
2021-12-20 19:59:32 -0800

    Completed password reset functionality.
    Added additional aesthetic touches on login screen.

--- DashCam/Controllers/ResetPasswordViewController.swift
+++ DashCam/Controllers/ResetPasswordViewController.swift
@@ -19,11 +19,17 @@ class ResetPasswordViewController : UIViewController {
     
     @IBAction func sendEmailButton(_ sender: Any) {
         if let email = emailAddressTextField.text{
-            Auth.auth().sendPasswordReset(withEmail: email) { error in
+            Auth.auth().sendPasswordReset(withEmail: email) { (error) in
                 if let e = error {
                     let message = e.localizedDescription
                     let alert = UIAlertController(title: "Oh no!", message: message, preferredStyle: .alert)
                     alert.addAction(UIAlertAction(title: "Try Again", style: .default, handler: nil))
+                    self.present(alert, animated: true, completion: nil)
+                }
+                else {
+                    self.performSegue(withIdentifier: "returnToLogin", sender: self)
+                    let alert = UIAlertController(title:"User found! Please reset your password!", message:"An email has been sent to " + email + ".", preferredStyle: .alert)
+                    alert.addAction(UIAlertAction(title: "Continue to Login", style: .default, handler: nil))
                     self.present(alert, animated: true, completion: nil)
                 }
                 
